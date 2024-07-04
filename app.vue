397fc02a697f2cc7e42ac538d1007c8140691894
Allan Rodriguez <0x.allan@gmail.com>
2024-07-04 14:12:33 -0600

    feat: init project

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container mx-auto">
+    <span>Hello World!</span>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+import Aura from '@primevue/themes/aura';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  ssr: false,
+  modules: ['@primevue/nuxt-module'],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  primevue: {
+    autoImport: true,
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primevue',
+            order: 'tailwind-base, primevue, tailwind-utilities'
+          }
+        },
+      }
+    },
+    components: {
+      prefix: 'P',
+      include: '*',
+    },
+    directives: {
+      prefix: 'p',
+      include: '*',
+    },
+  }
+})
