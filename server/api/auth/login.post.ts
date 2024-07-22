4fb934cf7b9ee1c17a2ceb65678ab40a076eedc1
Allan Rodriguez <aerodriguez@mintrabajo.gob.gt>
2024-07-22 15:41:55 -0600

    feat: add server config

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,7 +1,14 @@
+import { z } from 'zod'
+
 import { useApi } from '~/server/utils/useApi'
 
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
 export default defineEventHandler(async (event) => {
-  const data = await readBody(event)
+  const data = await readValidatedBody(event, schema.parse)
 
   return useApi('/api/v1/auth/login', {
     method: 'POST',
