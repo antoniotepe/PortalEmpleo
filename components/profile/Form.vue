bde9ad2883378bc2a0f59c63965320054e979bfc
Mynor Saban <msaban@mintrabajo.gob.gt>
2024-07-26 16:54:54 -0600

    feat:add page profile fix's

--- components/profile/Form.vue
+++ components/profile/Form.vue
@@ -1,68 +1,52 @@
-import { NuxtLink } from '../../.nuxt/components';
-<script  setup lang="ts">
-
-const InitProfileData={
-       FirstName:'Ana',
-       SecondName:'Cristina',
-       FirstSurname:'Aguirre',
-       SecondSurname:'de León',
-       ThirdName:'Eloisa',
-       LastNameMarried:'García',
-       Cui:'3279501101',
-
-}
-let Profile=true;
-const ProfileData=reactive({...InitProfileData});
-const OriginalProfileData=ref({...InitProfileData});
+<script lang="ts" setup>
+const profileView = ref(false)
 const isEditing = ref(false)
-const cuiEdit=ref(false);
-const DateMailAdded=ref('Agregado hace 1 mes');
+// const DateMailAdded = ref('Agregado hace 1 mes')
+
+const InitProfileData = {
+  firstName: 'Ana',
+  secondName: 'Cristina',
+  thirdName: 'Eloisa',
+  firstSurname: 'Aguirre',
+  secondSurname: 'de León',
+  lastNameMarried: 'García',
+  cui: '3279501101',
+}
+
+const ProfileData = reactive({ ...InitProfileData })
+const OriginalProfileData = ref({ ...InitProfileData }) // esta es la copia
+
+const cancelEdit = () => {
+  isEditing.value = false
+  if (isEditing.value === false) {
+    Object.assign(ProfileData, OriginalProfileData.value)
+    console.log(ProfileData.firstName)
+  }
+}
+// interface FormData {
+//   firstName: string
+//   email: string
+// }
+
+// const form: FormData = reactive({
+//   firstName: 'Amy Elsner Reyes',
+//   email: 'amy.elsner@gmail.com',
+// })
+
 const toggleEdit = () => {
   isEditing.value = !isEditing.value
   if (!isEditing.value) {
-
-  
-
-    
     // Aquí puedes agregar la lógica para guardar los cambios
   }
-
 }
- const cancelEdit = () => {
-   isEditing.value = false
-
-   if(isEditing.value===false){
-      Object.assign(ProfileData, OriginalProfileData.value);
-    console.log(ProfileData.FirstName)
-   }
-  }
- interface FormData {
-   firstName: string
-   email: string
-  }
-
-  const form:FormData=reactive({
-   firstName: 'Amy Elsner Reyes',
-   email: 'amy.elsner@gmail.com',
-
-  })
-  onBeforeMount(()=>{
-  console.log('datos son los siguientes',form.firstName)
-
-  })
-
-const AddEmail=()=>{
-  const Datos=false;
-  Profile=Datos;
-}
-  
 </script>
+
 <template>
   <div class="flex flex-col gap-4">
     <p-card>
       <template #content>
-        <div class="flex items-center justify-between font-normal"  >
-          <div class="flex gap-2" >
+        <div class="flex items-center justify-between font-normal">
+          <div class="flex gap-2">
             <p-avatar
               image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
               size="xlarge"
@@ -70,23 +54,36 @@ const AddEmail=()=>{
             />
 
             <div class="flex flex-col justify-center">
-              <h1 class="text-2xl font-bold"> {{form.firstName}} </h1>
-              <p class="text-subtitle"> {{ form.email}}</p>
+              <h1 class="text-2xl font-bold">Amy Elsner</h1>
+              <p class="text-subtitle">amy.elsner@gmail.com</p>
             </div>
           </div>
-
-          <div class="flex flex-col items-center gap-2 sm:flex-row" >
+          <div v-if="profileView">
             <p-button
               icon="pi pi-pencil"
-              class="bg-[#142958] px-5 text-white redondeado-lg border-0"
-              :label="isEditing ? $t('ui.buttons.save.label'): $t('ui.buttons.edit.label')"
+              rounded
+              class="bg-[#142958]"
+              :label="$t('ui.buttons.return.label')"
+              @click="toggleEdit"
+            />
+          </div>
+          <div
+            v-else
+            class="flex flex-col items-center gap-2 sm:flex-row"
+          >
+            <p-button
+              icon="pi pi-pencil"
+              rounded
+              class="bg-[#142958]"
+              :label="isEditing ? $t('ui.buttons.save.label') : $t('ui.buttons.edit.label')"
               @click="toggleEdit"
             />
             <p-button
               v-if="isEditing"
               icon="pi pi-times"
-              class="bg-[#142958] px-5 text-white redondeado-lg border-0"
-              :label="$t('ui.buttons.cancel.label')"
+              class="text-red-600"
+              rounded
+              :label="$t('ui.buttons.cancel')"
               link
               @click="cancelEdit"
             />
@@ -96,59 +93,99 @@ const AddEmail=()=>{
     </p-card>
 
     <p-card>
-      <!-- <template #title>{{ $t('profile.personalInformation') }}</template> -->
+      <template #title>{{ $t('profile.personalInformation') }}</template>
 
       <template #content>
-        <div v-if="Profile" class="w-full flex gap-4">
+        <div v-if="profileView">
+          <div class="grid grid-cols-3 gap-4">
+            <form>
+              <div class="flex flex-col gap-2">
+                <label for="cui"> Correo Electrónico </label>
 
-          <div class="w-1/2 flex flex-col gap-4" >
+                <p-input-text
+                  id="cui"
+                  type="email"
+                  filled
+                  fluid
+                  placeholder="ingresa tu correo"
+                  required
+                />
+              </div>
+
+              <p-button
+                rounded
+                class="text-secondary bg-[#142958]"
+                type="submit"
+                :label="registrars"
+                >dfadf</p-button
+              >
+            </form>
+          </div>
+        </div>
+        <div
+          v-else
+          class="flex flex-col gap-4"
+        >
+          <div class="grid grid-cols-3 gap-4">
+            <div class="flex flex-col gap-2">
+              <label for="cui"> {{ $t('profile.uniqueIdentificationCode') }} </label>
+
+              <p-input-text
+                id="cui"
+                v-model="ProfileData.cui"
+                filled
+                fluid
+                disabled="false"
+              />
+            </div>
+          </div>
+
+          <div class="grid grid-cols-3 gap-4">
             <div class="flex flex-col gap-2">
               <label for="firstName"> {{ $t('profile.firstName') }} </label>
 
               <p-input-text
                 id="firstName"
-                :value="ProfileData.FirstName"
-                filled
-                v-model="ProfileData.FirstName"
-                fluid
-                :disabled="!isEditing"
-              />
-            </div>
-              <div class="flex flex-col gap-2">
-              <label for="firstSurname"> {{ $t('profile.firstSurname') }} </label>
-
-              <p-input-text
-                id="firstSurname"
-                :value="ProfileData.FirstSurname"
-                v-model="ProfileData.FirstSurname"
+                v-model="ProfileData.firstName"
                 filled
                 fluid
                 :disabled="!isEditing"
               />
             </div>
-             <div class="flex flex-col gap-2">
-              <label for="marriedSurname"> {{ $t('profile.marriedName') }} </label>
 
-              <p-input-text
-                id="marriedSurname"
-                :value="ProfileData.LastNameMarried"
-                v-model="ProfileData.LastNameMarried"
-                filled
-                fluid
-                :disabled="!isEditing"
-              />
-            </div>           
-          </div>
-
-          <div class=" w-1/2 flex flex-col gap-4" >
-              <div class="flex flex-col gap-2" >
+            <div class="flex flex-col gap-2">
               <label for="secondName"> {{ $t('profile.middleName') }} </label>
 
               <p-input-text
                 id="secondName"
-                :value="ProfileData.SecondName"
+                v-model="ProfileData.secondName"
                 filled
-                v-model="ProfileData.SecondName"
+                fluid
+                :disabled="!isEditing"
+              />
+            </div>
+
+            <div class="flex flex-col gap-2">
+              <label for="thirdName"> {{ $t('profile.thirdName') }} </label>
+
+              <p-input-text
+                id="thirdName"
+                v-model="ProfileData.thirdName"
+                filled
+                fluid
+                :disabled="!isEditing"
+              />
+            </div>
+          </div>
+
+          <div class="grid grid-cols-3 gap-4">
+            <div class="flex flex-col gap-2">
+              <label for="firstSurname"> {{ $t('profile.firstSurname') }} </label>
+
+              <p-input-text
+                id="firstSurname"
+                v-model="ProfileData.firstSurname"
+                filled
                 fluid
                 :disabled="!isEditing"
               />
@@ -159,33 +196,25 @@ const AddEmail=()=>{
 
               <p-input-text
                 id="secondSurname"
-                :value="ProfileData.SecondSurname"
-                v-model="ProfileData.SecondSurname"
+                v-model="ProfileData.secondSurname"
                 filled
                 fluid
                 :disabled="!isEditing"
               />
             </div>
 
-            <div class="flex flex-col gap-2" >
             <div class="flex flex-col gap-2">
-              <label for="cui"> {{ $t('profile.uniqueIdentificationCode') }} </label>
+              <label for="marriedSurname"> {{ $t('profile.marriedName') }} </label>
 
               <p-input-text
-                id="cui"
-                :value="ProfileData.Cui"
+                id="marriedSurname"
+                v-model="ProfileData.lastNameMarried"
                 filled
                 fluid
-                v-model='ProfileData.Cui'
-                :disabled="!cuiEdit"
+                :disabled="!isEditing"
               />
             </div>
           </div>
-          </div>
-        </div>
-        <div class="w-full flex gap-4" v-else>
-          <h1>hola</h1>
-
         </div>
       </template>
     </p-card>
@@ -194,28 +223,7 @@ const AddEmail=()=>{
       <template #title>{{ $t('profile.notifications') }}</template>
 
       <template #content>
-        <div class="flex flex-col gap-4 " >
-          <div class="flex flex-wrap gap-2 justify-start" >
-            <div class="flex">
-               <div class="flex place-items-center p-6 rounded-full bg-[#ECF3FF]">
-                <i class="pi pi-envelope text-[#ffffff]  bg-[#4182F9]"/>
-              </div>
-              <div class="ml-2">
-                <p>{{form.email}}</p>
-              <p class=" text-black opacity-60"> {{DateMailAdded}} </p>
-              </div>
-            </div>
-
-          </div>
-          <div class="">
-             <p-button
-              icon="pi pi-plus"
-              class=" bg-[#ECF3FF] px-5 redondeado-lg text-[#4182F9] border-0"
-              :label=" $t('ui.buttons.AddNewMail.label')"
-              @click="AddEmail"
-            />
-          </div>
-        </div>
+        <span>{{ $t('profile.mailingList') }}</span>
       </template>
     </p-card>
   </div>
