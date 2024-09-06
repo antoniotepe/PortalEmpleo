149cef63d3affaa7792cc93b550cce091db61166
Edwin Antonio Sion Tepe <tepe1600@gmail.com>
2024-09-06 09:46:56 -0600

    Se elimino la autotizacion

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,11 +2,11 @@ export default defineNuxtRouteMiddleware((to) => {
   const auth = useAuthStore()
   auth.init()
 
-  if (to.path.includes('/auth') && auth.access) {
+  if (to.path.includes('/auth') ) {
     return navigateTo('/')
   }
 
-  if (!to.path.includes('/auth') && !auth.access) {
-    return navigateTo('/auth/login')
-  }
+  // if (!to.path.includes('/auth') && !auth.access) {
+  //   return navigateTo('/auth/login')
+  // }
 })
